c317ccebe150240c4f8c1da408cb9bb89dce7282
TheBlackParrot <theblackparrot@rocketmail.com>
2015-02-09 01:09:59 -0600

    pushing up what's done of liquid detection

--- liquid_functions.cs
+++ liquid_functions.cs
@@ -23,13 +23,26 @@ function Player::doLiquidLoop(%this) {
 
 	// ugh
 	// can't %array[%another[1]] and it SUCKS
-	%player_pos_xf = %player_pos[x] - %player_pos[x] % 4;
-	%player_pos_yf = %player_pos[y] - %player_pos[y] % 4;
-	%player_pos_zf = %player_pos[z] - %player_pos[z] % 4; 
+	if(%player_pos[x] < 0) {
+		%player_pos_xf = mCeil(%player_pos[x] - (%player_pos[x] % 4));
+	} else {
+		%player_pos_xf = mFloor(%player_pos[x] - (%player_pos[x] % 4));
+	}
+	if(%player_pos[y] < 0) {
+		%player_pos_yf = mCeil(%player_pos[y] - (%player_pos[y] % 4));
+	} else {
+		%player_pos_yf = mFloor(%player_pos[y] - (%player_pos[y] % 4));
+	}
+	if(%player_pos[z] < 0) {
+		%player_pos_zf = mCeil(%player_pos[z] - (%player_pos[z] % 4));
+	} else {
+		%player_pos_zf = mFloor(%player_pos[z] - (%player_pos[z] % 4));
+	}
 
 	%brick = $Mining::Brick[%player_pos_xf,%player_pos_yf,%player_pos_zf];
 
-	if(%brick.brickType != "liquid" || !isObject(%brick)) {
+	if(!isObject(%brick)) {
+		talk("NOT OBJECT" SPC %brick SPC "$Mining::Brick[" @ %player_pos_xf @ "," @ %player_pos_yf @ "," @ %player_pos_zf @ "]" SPC %this.getPosition());
 		return;
 	}
 
@@ -41,7 +54,11 @@ function Player::doLiquidLoop(%this) {
 	%brick_box[y2] = getWord(%brick_box,4);
 	%brick_box[z2] = getWord(%brick_box,5);
 
-	if(%player_pos[x] >= %brick_box[x1] && %player_pos[y] >= %brick_box[y1] && %player_pos[z] >= %brick_box[z1] && %player_pos[x] <= %brick_box[x2] && %player_pos[y] <= %brick_box[y2] && %player_pos[z] <= %brick_box[z2]) {
-		talk("IN" SPC %brick);
+	if(%player_pos_xf >= %brick_box[x1] && %player_pos_yf >= %brick_box[y1] && %player_pos_zf >= %brick_box[z1] && %player_pos_xf <= %brick_box[x2] && %player_pos_yf <= %brick_box[y2] && %player_pos_zf <= %brick_box[z2]) {
+		if(%brick.liquidObj != -1) {
+			talk("IN" SPC %brick SPC "$Mining::Brick[" @ %player_pos_xf @ "," @ %player_pos_yf @ "," @ %player_pos_zf @ "]" SPC %this.getPosition());
+		} else {
+			talk("NOT LIQUID" SPC "$Mining::Brick[" @ %player_pos_xf @ "," @ %player_pos_yf @ "," @ %player_pos_zf @ "]" SPC %this.getPosition());
+		}
 	}
 }
